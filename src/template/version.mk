70e03a1b1d06122ba8a4654586a86028fd76a6dd
Mike Bobadilla <mike.c.bobadilla@gmail.com>
2019-04-09 17:16:45 +0000

    INITIAL

---
+++ src/template/version.mk
@@ -0,0 +1,2 @@
+NAME	= essentials
+VERSION	= 0.1.0
---
+++ version.mk
@@ -0,0 +1,6 @@
+export ROLL	= essentials
+export VERSION	= 0.1.0
+export ROLLVERSION = 0.1.0
+export RELEASE	= $(shell $(STACKBUILD.ABSOLUTE)/bin/os-release)
+ROLLS.OS	= sles
+ISOSIZE		= 0
